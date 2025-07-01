12f503900077e6d5d148c457f0db85161747a0a2
Balaveera Thatikonda <balaveer@gmail.com>
2025-07-01 08:53:07 -0400

    Update create-log-analytics-workspace.tf

--- cloud/azure/IaC/create-log-analytics-workspace.tf
+++ cloud/azure/IaC/create-log-analytics-workspace.tf
@@ -20,3 +20,31 @@ resource "azurerm_log_analytics_workspace" "example" {
   location            = "eastus" # Or your desired location
   sku                 = "PerGB2018" # Or your desired SKU (e.g., Free, Premium, etc.)
 }
+
+
+
+#==============MAIN - LAW & RG ======= START
+
+locals{
+common_tags = {
+    application_name     = "s-assetmamager"
+  	data_classification="internal"
+	environment="dev"
+	owner_email="bthati@abc.com"
+  }
+  }
+
+# Create a resource group using the generated random name
+resource "azurerm_resource_group" "rg_data" {
+  name     = "rg-s-backend-003"
+  location = "eastus"
+  tags = local.common_tags
+}
+
+resource "azurerm_log_analytics_workspace" "law_data" {
+  name                = "law-s-backend-003"
+  location            = azurerm_resource_group.rg_data.location
+  resource_group_name = azurerm_resource_group.rg_data.name
+  sku                 = "PerGB2018"
+}
+#==============MAIN - LAW & RG ======= END
